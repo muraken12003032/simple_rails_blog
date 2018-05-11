662deffa8cb8ef5e4a0b1ffcc64c0cbc4c74a7d6
Ken Murata <k_murata@nttpc.co.jp>
2018-05-11 00:13:31 +0000

    second fommit

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,8 +1,8 @@
 Recaptcha.configure do |config|
-  #config.site_key = '6LdWllgUAAAAAEGb3Dwo_Z7eJID7BjxA7Z1vkqto'
-  #config.secret_key = '6LdWllgUAAAAAJ8T3p0cJO_I3dHfX2t46SNIF3qk'
-  config.site_key = ENV["RECAPTCHA_SITEKEY"]
-  config.secret_key = ENV["RECAPTCHA_SECRETKEY"]
+  config.site_key = '6LdWllgUAAAAAEGb3Dwo_Z7eJID7BjxA7Z1vkqto'
+  config.secret_key = '6LdWllgUAAAAAJ8T3p0cJO_I3dHfX2t46SNIF3qk'
+  #config.site_key = ENV["RECAPTCHA_SITEKEY"]
+  #config.secret_key = ENV["RECAPTCHA_SECRETKEY"]
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
